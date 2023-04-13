846b1dbedaf76c7a8483d6d312ff5a189d573054
Vinicius De Martin <demartinvinicius@gmail.com>
2023-04-13 08:15:43 -0300

    first ommit on

---
+++ mappertests/OutputModel/OutputEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace mappertests.OutputModel;
+
+internal class OutputEmployee
+{
+    [JsonPropertyName("name")]
+    [JsonRequired]
+    public string Name { get; set; }
+
+
+}
